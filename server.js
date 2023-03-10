5c9a3620504c2bb614cc86837e7f6c1c7ee9aed7
reedoooo <readvogt@gmail.com>
2023-03-10 10:24:07 -0700

    ddd

--- server.js
+++ server.js
@@ -9,6 +9,15 @@ app.use(cors());
 
 const PORT = process.env.PORT || 3001;
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 const projects = require("./data/projects.json");
 const profile = require("./data/profile.json");
 
