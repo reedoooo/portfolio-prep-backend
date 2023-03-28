4e8c6e7cc2c1198873bb232adba5f84a06efd380
Reed Vogt <readvogt@gmail.com>
2023-03-28 15:38:26 -0700

    really frustrating session, accomplished nothing

---
+++ https.js
@@ -0,0 +1,14 @@
+const https = require('https');
+const fs = require('fs');
+
+const options = {
+  key: fs.readFileSync('server.key'),
+  cert: fs.readFileSync('server.crt')
+};
+
+const server = https.createServer(options, (req, res) => {
+  res.writeHead(200);
+  res.end('Hello, world!');
+});
+
+server.listen(3000);
--- server.js
+++ server.js
@@ -41,22 +41,13 @@ app.get('/', (req, res) => {
   res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
 });
 
-// Route to retrieve user information
-// app.get('/userinfo', checkJwt, (req, res) => {
-//   res.send(req.user);
-// });
-
-// app.get('/userinfo', checkJwt, (req, res) => {
-//   res.send(req.user);
-// });
-
-
 app.use('/users', users)
 app.get('/profile', requiresAuth(), (req, res) => {
   res.send(JSON.stringify(req.oidc.user));
 });
 
 app.use('/myprofile', profileData);
+// app.get('/myprofile', profileData);
 
 app.use(function (req, res, next) {
   res.locals.user = req.oidc.user;
@@ -91,3 +82,93 @@ mongoose
   .catch((error) => {
     console.log(error);
   });
+
+
+//   const dotenv = require("dotenv");
+// const express = require("express");
+// const logger = require("morgan");
+// const path = require("path");
+// const router = require("./routes/index");
+// const cors = require("cors");
+// const mongoose = require("mongoose");
+// const profileData = require("./routes/myprofile");
+// const { auth, requiresAuth } = require("express-openid-connect");
+// const users = require("./routes/users");
+// dotenv.config();
+
+// const app = express();
+
+// app.set("views", path.join(__dirname, "views"));
+// app.set("view engine", "ejs");
+
+// app.use(cors());
+// app.use(logger("dev"));
+// app.use(express.static(path.join(__dirname, "public")));
+// app.use(express.json());
+// const port = process.env.PORT || 3001;
+
+// const config = {
+//   baseURL:
+//     process.env.NODE_ENV === "production"
+//       ? "https://reedthamosthuman.onrender.com"
+//       : `http://localhost:${port}`,
+//   clientID: "pkjVpsG2T7vvDJ8YVpQ8AGippZ8MAJsn",
+//   issuerBaseURL: "https://dev-eq6zzpz5vj8o8v17.us.auth0.com",
+//   secret: "X11cUZbZdGR7TxzEfSWuVxfbT6TjdoEmJFJfKseEwZdB3FV3GbMOXc75Tl8alwxC",
+// };
+
+// if (!config.baseURL && port && process.env.NODE_ENV !== "production") {
+//   config.baseURL = [
+//     `http://localhost:${port}`,
+//     "https://reedthamosthuman.onrender.com",
+//   ];
+// }
+
+// app.use(auth(config));
+
+// app.get("/", (req, res) => {
+//   res.send(req.oidc.isAuthenticated() ? "Logged in" : "Logged out");
+// });
+
+// app.use("/users", users);
+// app.get("/profile", requiresAuth(), (req, res) => {
+//   res.send(JSON.stringify(req.oidc.user));
+// });
+
+// app.get("/myprofile", profileData);
+
+// app.use(function (req, res, next) {
+//   res.locals.user = req.oidc.user;
+//   next();
+// });
+
+// app.use("/", router);
+
+// app.use(function (req, res, next) {
+//   const err = new Error("Not Found");
+//   err.status = 404;
+//   next(err);
+// });
+
+// app.use(function (err, req, res, next) {
+//   res.status(err.status || 500);
+//   res.render("error", {
+//     message: err.message,
+//     error: process.env.NODE_ENV !== "production" ? err : {},
+//   });
+// });
+
+// const DATABASE_URL = process.env.DATABASE_URL;
+
+// mongoose
+//   .connect(DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+//   .then(() => {
+//     app.listen(port, () => {
+//       console.log(
+//         `Connected to MongoDB and listening... on ${config.baseURL}`
+//       );
+//     });
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   });
